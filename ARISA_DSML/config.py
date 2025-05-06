b5488ce95cefea87fd2d86e7a46e135ea58009ef
Jakub Wrona <kuba.wrona@onet.pl>
2025-05-06 19:10:33 +0000

    Implement structural updates and optimizations across multiple modules

--- ARISA_DSML/config.py
+++ ARISA_DSML/config.py
@@ -10,11 +10,14 @@ load_dotenv()
 PROJ_ROOT = Path(__file__).resolve().parents[1]
 logger.info(f"PROJ_ROOT path is: {PROJ_ROOT}")
 
+DATASET = "fedesoriano/stroke-prediction-dataset"
+
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 INTERIM_DATA_DIR = DATA_DIR / "interim"
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
+
 
 MODELS_DIR = PROJ_ROOT / "models"
 
