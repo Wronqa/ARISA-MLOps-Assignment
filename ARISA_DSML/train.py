c700291b89edba9b06b348ffb491edfdab190057
Jakub Wrona <kuba.wrona@onet.pl>
2025-05-07 18:02:27 +0000

    Update retrain workflow to include task-02-mlops branch and modify print statement for testing

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -331,7 +331,7 @@ def get_or_create_experiment(experiment_name: str):
 
 if __name__ == "__main__":
 
-    print('Comment for test starting github actions')
+    print('Comment for test starting github actions 2')
 
     df_train = pd.read_csv(PROCESSED_DATA_DIR / "train.csv")
 
