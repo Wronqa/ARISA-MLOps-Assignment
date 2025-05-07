c0a82d5cb0e11f2bc1ff9fdd901b4476cdfb647b
Jakub Wrona <kuba.wrona@onet.pl>
2025-05-07 19:00:53 +0000

    Add missing import for os and set MLflow tracking URI

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -1,4 +1,5 @@
 import typer
+import os
 from pathlib import Path
 
 from catboost import CatBoostClassifier, Pool, cv
@@ -23,6 +24,9 @@ from ARISA_DSML.helpers import get_git_commit_hash
 import nannyml as nml
 
 app = typer.Typer()
+
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI"))
 
 
 def run_hyperopt(X_train: pd.DataFrame, y_train: pd.DataFrame, test_size: float = 0.25, n_trials: int = 20, overwrite: bool = False) -> str | Path:  # noqa: PLR0913
