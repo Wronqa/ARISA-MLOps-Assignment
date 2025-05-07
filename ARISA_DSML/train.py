aeda36c40be0ba9cabde7496b5315f560537d33d
Jakub Wrona <kuba.wrona@onet.pl>
2025-05-07 20:13:15 +0000

    Update test comment for GitHub Actions

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -332,7 +332,7 @@ def get_or_create_experiment(experiment_name: str):
 
 if __name__ == "__main__":
 
-    print('Comment for test starting github actions 9')
+    print('Comment for test starting github actions 10')
 
     df_train = pd.read_csv(PROCESSED_DATA_DIR / "train.csv")
 
