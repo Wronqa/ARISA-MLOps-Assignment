89d3e875ec81afc40c67585eb5d770083e3a5394
Jakub Wrona <kuba.wrona@onet.pl>
2025-05-07 19:51:44 +0000

    Update test comment in main execution block for clarity

--- ARISA_DSML/train.py
+++ ARISA_DSML/train.py
@@ -332,7 +332,7 @@ def get_or_create_experiment(experiment_name: str):
 
 if __name__ == "__main__":
 
-    print('Comment for test starting github actions 6')
+    print('Comment for test starting github actions 7')
 
     df_train = pd.read_csv(PROCESSED_DATA_DIR / "train.csv")
 
